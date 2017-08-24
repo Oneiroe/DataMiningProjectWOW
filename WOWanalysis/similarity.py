551ec16865fc66afdeb8e4a689b13aa3da80e902
Alessio Cecconi <alessio.cecconi.1991@gmail.com>
2017-08-24 15:08:40 +0200

    Similarity: distance matrix plotting; bugfix; refactoring

--- WOWanalysis/similarity.py
+++ WOWanalysis/similarity.py
@@ -12,6 +12,8 @@ import logging
 from tqdm import tqdm
 import json
 import csv
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 ###############################################
@@ -74,11 +76,6 @@ def characters_stats_max_min_mean(DB_BASE_PATH, locations):
     return maximum, minimum, mean
 
 
-def max_distance(vector_max, vector_min):
-    """ Retrieve the maximal distance between two characters: MAX + MIN + MAX/1000 """
-    return euclidean_distance(vector_max, vector_min)
-
-
 ###############################################
 # GENERAL DISTANCE FUNCTIONS
 ##############
@@ -86,6 +83,8 @@ def max_distance(vector_max, vector_min):
 def jaccard_distance(set_1, set_2):
     """ Jaccard distance is 1 minus the ratio of the sizes of the intersection and union of set_1 and set_2 """
     logging.debug('jaccard_distance(' + str(set_1) + ', ' + str(set_2) + ')')
+    if len(set_1.union(set_2)) == 0:
+        return 0
     return 1 - len(set_1.intersection(set_2)) / len(set_1.union(set_2))
 
 
@@ -293,7 +292,7 @@ def distance_stats(character_1_json_path, character_2_json_path):
     return distance
 
 
-def distance_stats_normalized(character_1_json_path, character_2_json_path, max_distance):
+def distance_stats_normalized(character_1_json_path, character_2_json_path, max_distance=5990271.526328605):
     """ Return the distance measure between the two given characters according to their "stats" fields """
     logging.debug('distance_stats(' + str(character_1_json_path) + ', ' + str(character_2_json_path) + ')')
     distance = 0.0
@@ -311,8 +310,6 @@ def distance_stats_normalized(character_1_json_path, character_2_json_path, max_
                 stats_2 = list(stats_2.values())
                 # Normalization 0<=distance<=1
                 distance = euclidean_distance_normalized(stats_1, stats_2, max_distance)
-
-
             except KeyError as err:
                 logging.warning('KeyError: ' + str(err) + ' -- line: ' + str(sys.exc_info()[-1].tb_lineno))
             except ValueError as err:
@@ -357,9 +354,29 @@ def distance_talents(character_1_json_path, character_2_json_path):
 
 
 ###############################################
+# VISUALIZATION
+##############
+def show_distance_matrix(characters_iterator, distance_function):
+    d = np.array([distance_function(x, y) for x in characters_iterator for y in characters_iterator])
+    plt.matshow(d.reshape(len(characters_iterator), len(characters_iterator)), cmap="Reds")
+    plt.colorbar()
+    plt.show()
+
+
+###############################################
 # TEST-MAIN
 ##############
 def main():
+    ###############################################
+    ####
+    # LOG setup
+    logging.basicConfig(filename=os.path.join(os.getcwd(), 'LOG', 'similarity_DEBUG.log'),
+                        level=logging.DEBUG,
+                        format='%(asctime)-15s '
+                               '%(levelname)s '
+                               '--%(filename)s-- '
+                               '%(message)s')
+
     DB_BASE_PATH = os.path.join(os.getcwd(), 'DB', 'WOW')
     locations = {
         'EU': ['en_GB', 'de_DE', 'es_ES', 'fr_FR', 'it_IT', 'pt_PT', 'ru_RU'],
@@ -368,7 +385,52 @@ def main():
         'US': ['en_US', 'pt_BR', 'es_MX']
     }
 
-    maximum, minimum, mean = characters_stats_max_min_mean(DB_BASE_PATH, locations)
+    # maximum, minimum, mean = characters_stats_max_min_mean(DB_BASE_PATH, locations)
+    stats_maximum_global = {'agi': 44923, 'armor': 78232, 'avoidanceRating': 892.0, 'avoidanceRatingBonus': 20.0,
+                      'block': 129.9673, 'blockRating': 0, 'crit': 134.29715, 'critRating': 24400, 'dodge': 1103.7595,
+                      'dodgeRating': 11543, 'haste': 132.06, 'hasteRating': 19220, 'hasteRatingPercent': 115.55567,
+                      'health': 5693023, 'int': 46425, 'leech': 102.430435, 'leechRating': 878.0,
+                      'leechRatingBonus': 20.25, 'mainHandDmgMax': 100274.0, 'mainHandDmgMin': 90031.0,
+                      'mainHandDps': 58630.99, 'mainHandSpeed': 3.8, 'mana5': 148265.0, 'mana5Combat': 148265.0,
+                      'mastery': 418.2, 'masteryRating': 25380, 'offHandDmgMax': 40387.0, 'offHandDmgMin': 38875.0,
+                      'offHandDps': 27425.393, 'offHandSpeed': 3.8, 'parry': 116.17902, 'parryRating': 16055,
+                      'power': 1838164, 'rangedDmgMax': 60618.0, 'rangedDmgMin': 51016.0, 'rangedDps': 23017.041,
+                      'rangedSpeed': 3.253, 'speedRating': 1089.0, 'speedRatingBonus': 18.596735,
+                      'spellCrit': 134.29715, 'spellCritRating': 24400, 'spellPen': 0, 'sta': 82710, 'str': 36373,
+                      'versatility': 12720, 'versatilityDamageDoneBonus': 49.86, 'versatilityDamageTakenBonus': 24.93,
+                      'versatilityHealingDoneBonus': 49.86}
+    stats_minimum_global = {'agi': 5, 'armor': 0, 'avoidanceRating': 0.0, 'avoidanceRatingBonus': 0.0, 'block': 0.0,
+                      'blockRating': 0, 'crit': 0.0, 'critRating': 0, 'dodge': 0.0, 'dodgeRating': 0,
+                      'haste': -27.033333, 'hasteRating': 0, 'hasteRatingPercent': 0.0, 'health': 306, 'int': 9,
+                      'leech': -60.0, 'leechRating': -60.0, 'leechRatingBonus': 0.0, 'mainHandDmgMax': 1.0,
+                      'mainHandDmgMin': 0.0, 'mainHandDps': 0.125, 'mainHandSpeed': 0.667, 'mana5': 0.0,
+                      'mana5Combat': 0.0, 'mastery': 0.0, 'masteryRating': 0, 'offHandDmgMax': 0.0,
+                      'offHandDmgMin': 0.0, 'offHandDps': 0.0, 'offHandSpeed': 0.667, 'parry': 0.0, 'parryRating': 0,
+                      'power': 0, 'rangedDmgMax': -1.0, 'rangedDmgMin': -1.0, 'rangedDps': -1.0, 'rangedSpeed': -1.0,
+                      'speedRating': -30.518183, 'speedRatingBonus': 0.0, 'spellCrit': 0.0017, 'spellCritRating': 0,
+                      'spellPen': 0, 'sta': 17, 'str': 5, 'versatility': 0, 'versatilityDamageDoneBonus': 0.0,
+                      'versatilityDamageTakenBonus': 0.0, 'versatilityHealingDoneBonus': 0.0}
+    stats_mean_global = {'agi': 4672.277258699044, 'armor': 2001.4727582581563, 'avoidanceRating': 1.1033004669121393,
+                   'avoidanceRatingBonus': 0.6885062033465073, 'block': 1.3070891963135096, 'blockRating': 0.0,
+                   'crit': 19.73010125068563, 'critRating': 2254.2289264396663, 'dodge': 6.478166307476653,
+                   'dodgeRating': 25.950705419521128, 'haste': 16.06888523653173, 'hasteRating': 2158.5366572271587,
+                   'hasteRatingPercent': 13.908714036594885, 'health': 807942.6857703656, 'int': 7015.019343925931,
+                   'leech': 0.26359496126975535, 'leechRating': 1.0811484874981898,
+                   'leechRatingBonus': 0.2476907445228239, 'mainHandDmgMax': 10017.458861832734,
+                   'mainHandDmgMin': 8765.198996981619, 'mainHandDps': 3855.6615863063903,
+                   'mainHandSpeed': 2.2483025800377368, 'mana5': 9009.75571881571, 'mana5Combat': 8805.484450247575,
+                   'mastery': 49.83102065901402, 'masteryRating': 2645.372286847996,
+                   'offHandDmgMax': 3091.2397536118838, 'offHandDmgMin': 2875.3589754459745,
+                   'offHandDps': 1586.8783813258156, 'offHandSpeed': 1.8170947186799526, 'parry': 4.011504966751173,
+                   'parryRating': 114.33363714983382, 'power': 154321.02565200435, 'rangedDmgMax': 656.4463558977142,
+                   'rangedDmgMin': 628.8855558570169, 'rangedDps': 240.6216909360749,
+                   'rangedSpeed': -0.8339633597300509, 'speedRating': 28.23419487996765,
+                   'speedRatingBonus': 0.5558401780802803, 'spellCrit': 20.06153050474746,
+                   'spellCritRating': 2254.2282481516654, 'spellPen': 0.0, 'sta': 13278.711697924438,
+                   'str': 3836.495574170793, 'versatility': 1540.4860611815777,
+                   'versatilityDamageDoneBonus': 8.622942339860389, 'versatilityDamageTakenBonus': 4.311471171076854,
+                   'versatilityHealingDoneBonus': 8.622942339860389}
+    stats_max_dist_global = 5990271.526328605
 
 
 if __name__ == "__main__":
